3755c7dd7b845958c5e6b9769dea9f6fe8928598
Erhan Gündüz <erhangndz@live.com>
2023-05-27 13:30:26 +0300

    Add project files.

---
+++ EntityLayer/Concrete/SocialMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class SocialMedia
+    {
+        public int SocialMediaID { get; set; }
+        public DateTime Name { get; set; }
+        public string URL { get; set; }
+        public string Icon { get; set; }
+        public bool Status { get; set; }
+    }
+}
