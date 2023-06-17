812737758f94718892ce21486af981212e0da342
Erhan Gündüz <erhangndz@live.com>
2023-06-17 23:09:47 +0300

    lesson 178

---
+++ Core_Project/Controllers/ContactInfoController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ContactInfoController : Controller
+    {
+        private readonly IContactInfoService _contactInfoService;
+
+        public ContactInfoController(IContactInfoService contactInfoService)
+        {
+            _contactInfoService = contactInfoService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values= _contactInfoService.TGetByID(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactInfo p)
+        {
+            _contactInfoService.TUpdate(p);
+            return View();
+        }
+    }
+}
